7a155f2eea40bf87929c8ab6025267c01e544dd0
William Asaba <asabawilliamdk@yahoo.com>
2026-01-03 14:22:11 +0300

    feat: export new plugin modules
    - Added exports for new plugin modules: TextColorPlugin, BackgroundColorPlugin, and ImageUploadPlugin

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -8,3 +8,6 @@ export { InsertLinkPlugin } from './InsertLinkPlugin.js';
 export { StrikeThroughPlugin } from './StrikeThroughPlugin.js';
 export { RemoveLinkPlugin } from './RemoveLinkPlugin.js';
 export { TablePlugin } from './TablePlugin.js';
+export { TextColorPlugin } from './TextColorPlugin.js';
+export { BackgroundColorPlugin } from './BackgroundColorPlugin.js';
+export { ImageUploadPlugin } from './ImageUploadPlugin.js';
